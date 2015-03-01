0bbf868c063e15511e1247ee9504c02653f10250
Fedja Beader <specing@contact.me.on.irc.freenode.net>
2015-03-01 13:47:23 +0100

    Add reset/initial value setting to generic_register

--- generic_register.vhd
+++ generic_register.vhd
@@ -1,12 +1,13 @@
 library	ieee;
 use		ieee.std_logic_1164.all;
+use		ieee.numeric_std.all;
 
 
 
 entity generic_register is
 	generic
 	(
-		reset_value		: std_logic_vector := (others => '0')
+		reset_value		: integer := 0
 	);
 	port
 	(
@@ -22,15 +23,23 @@ end generic_register;
 
 architecture Behavioral of generic_register is
 
+	signal data_on_reset: std_logic_vector(data_i'length - 1 downto 0)
+						:=std_logic_vector(to_unsigned(reset_value, data_i'length));
+
+	signal data			: std_logic_vector(data_i'length - 1 downto 0)
+						:=data_on_reset;
+
 begin
+
+	data_o				<= data;
 
 	process (clock_i)
 	begin
 		if rising_edge (clock_i) then
 			if reset_i = '1' then
-				data_o <= reset_value;
+				data <= data_on_reset;
 			elsif clock_enable_i = '1' then
-				data_o <= data_i;
+				data <= data_i;
 			end if;
 		end if;
 	end process;
