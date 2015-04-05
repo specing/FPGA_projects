17e3cdc320e8d07108c50abd58b8b73c2efc75e1
Fedja Beader <specing@contact.me.on.irc.freenode.net>
2015-04-05 14:48:40 +0200

    block_descriptor -> tetrimino_type, change fsm

--- nexys4_tetris_demo/tetris_active_element.vhd
+++ nexys4_tetris_demo/tetris_active_element.vhd
@@ -31,6 +31,7 @@ entity tetris_active_element is
 		active_row_i				: in	std_logic_vector(integer(CEIL(LOG2(real(number_of_rows    - 1)))) - 1 downto 0);
 		active_column_i				: in	std_logic_vector(integer(CEIL(LOG2(real(number_of_columns - 1)))) - 1 downto 0);
 
+		-- communication with the main finite state machine
 		fsm_start_i					: in	std_logic;
 		fsm_ready_o					: out	std_logic
 	);
@@ -43,29 +44,29 @@ architecture Behavioral of tetris_active_element is
 	constant row_width					: integer := integer(CEIL(LOG2(real(number_of_rows    - 1))));
 	constant column_width				: integer := integer(CEIL(LOG2(real(number_of_columns - 1))));
 
-	-- block descriptor
-	constant block_descriptor_width		: integer := 3;
-	constant block_descriptor_empty 	: std_logic_vector := std_logic_vector(to_unsigned(0, block_descriptor_width));
+	-- tetrimino type
+	constant tetrimino_type_width		: integer := 3;
+	constant tetrimino_type_empty 		: std_logic_vector := std_logic_vector(to_unsigned(0, tetrimino_type_width));
 	-- ####
-	constant block_descriptor_pipe	 	: std_logic_vector := std_logic_vector(to_unsigned(1, block_descriptor_width));
+	constant tetrimino_type_pipe	 	: std_logic_vector := std_logic_vector(to_unsigned(1, tetrimino_type_width));
 	-- #
 	-- ###
-	constant block_descriptor_L_left	: std_logic_vector := std_logic_vector(to_unsigned(2, block_descriptor_width));
+	constant tetrimino_type_L_left		: std_logic_vector := std_logic_vector(to_unsigned(2, tetrimino_type_width));
 	--   #
 	-- ###
-	constant block_descriptor_L_right 	: std_logic_vector := std_logic_vector(to_unsigned(3, block_descriptor_width));
+	constant tetrimino_type_L_right 	: std_logic_vector := std_logic_vector(to_unsigned(3, tetrimino_type_width));
 	-- ##
 	--  ##
-	constant block_descriptor_Z_left 	: std_logic_vector := std_logic_vector(to_unsigned(4, block_descriptor_width));
+	constant tetrimino_type_Z_left 		: std_logic_vector := std_logic_vector(to_unsigned(4, tetrimino_type_width));
 	--  ##
 	-- ##
-	constant block_descriptor_Z_right 	: std_logic_vector := std_logic_vector(to_unsigned(5, block_descriptor_width));
+	constant tetrimino_type_Z_right 	: std_logic_vector := std_logic_vector(to_unsigned(5, tetrimino_type_width));
 	--  #
 	-- ###
-	constant block_descriptor_T			: std_logic_vector := std_logic_vector(to_unsigned(6, block_descriptor_width));
+	constant tetrimino_type_T			: std_logic_vector := std_logic_vector(to_unsigned(6, tetrimino_type_width));
 	-- ##
 	-- ##
-	constant block_descriptor_square	: std_logic_vector := std_logic_vector(to_unsigned(7, block_descriptor_width));
+	constant tetrimino_type_square		: std_logic_vector := std_logic_vector(to_unsigned(7, tetrimino_type_width));
 
 
 	constant default_pipe_row0			: std_logic_vector := "00001";
@@ -131,149 +132,189 @@ architecture Behavioral of tetris_active_element is
 	constant default_T_column2			: std_logic_vector := "0111";
 	constant default_T_column3			: std_logic_vector := "1000";
 
+	signal block0_row					: std_logic_vector (row_width - 1 downto 0);
+	signal block1_row					: std_logic_vector (row_width - 1 downto 0);
+	signal block2_row					: std_logic_vector (row_width - 1 downto 0);
+	signal block3_row					: std_logic_vector (row_width - 1 downto 0);
+	signal block0_column				: std_logic_vector (column_width - 1 downto 0);
+	signal block1_column				: std_logic_vector (column_width - 1 downto 0);
+	signal block2_column				: std_logic_vector (column_width - 1 downto 0);
+	signal block3_column				: std_logic_vector (column_width - 1 downto 0);
+	signal active_address_write_enable	: std_logic;
 
-	type fsm_states is
-	(
-		state_new_elem,
-		state_start
-	);
-	signal state, next_state			: fsm_states := state_new_elem;
+	signal block0_row_new				: std_logic_vector (row_width - 1 downto 0);
+	signal block1_row_new				: std_logic_vector (row_width - 1 downto 0);
+	signal block2_row_new				: std_logic_vector (row_width - 1 downto 0);
+	signal block3_row_new				: std_logic_vector (row_width - 1 downto 0);
+	signal block0_column_new			: std_logic_vector (column_width - 1 downto 0);
+	signal block1_column_new			: std_logic_vector (column_width - 1 downto 0);
+	signal block2_column_new			: std_logic_vector (column_width - 1 downto 0);
+	signal block3_column_new			: std_logic_vector (column_width - 1 downto 0);
+	signal new_address_write_enable		: std_logic;
 
+	signal block0_row_next				: std_logic_vector (row_width - 1 downto 0);
+	signal block1_row_next				: std_logic_vector (row_width - 1 downto 0);
+	signal block2_row_next				: std_logic_vector (row_width - 1 downto 0);
+	signal block3_row_next				: std_logic_vector (row_width - 1 downto 0);
+	signal block0_column_next			: std_logic_vector (column_width - 1 downto 0);
+	signal block1_column_next			: std_logic_vector (column_width - 1 downto 0);
+	signal block2_column_next			: std_logic_vector (column_width - 1 downto 0);
+	signal block3_column_next			: std_logic_vector (column_width - 1 downto 0);
 
-	type rotation_enum is
-	(
-		ROTATION_0,
-		ROTATION_90,
-		ROTATION_180,
-		ROTATION_270
-	);
+	signal block0_row_operand			: std_logic_vector (row_width - 1 downto 0);
+	signal block1_row_operand			: std_logic_vector (row_width - 1 downto 0);
+	signal block2_row_operand			: std_logic_vector (row_width - 1 downto 0);
+	signal block3_row_operand			: std_logic_vector (row_width - 1 downto 0);
+	signal block0_column_operand		: std_logic_vector (column_width - 1 downto 0);
+	signal block1_column_operand		: std_logic_vector (column_width - 1 downto 0);
+	signal block2_column_operand		: std_logic_vector (column_width - 1 downto 0);
+	signal block3_column_operand		: std_logic_vector (column_width - 1 downto 0);
 
 	type operation_enum is
 	(
-		OP_NULL,
-		OP_NEW,
-		OP_MOVE_LEFT,
-		OP_MOVE_RIGHT,
-		OP_ROT_LEFT,
-		OP_ROT_RIGHT,
-		OP_MOVE_DOWN
+		ZERO,
+		PLUS_ONE,
+		MINUS_ONE
 	);
+	signal block0_row_operation			: operation_enum;
+	signal block1_row_operation			: operation_enum;
+	signal block2_row_operation			: operation_enum;
+	signal block3_row_operation			: operation_enum;
+	signal block0_column_operation		: operation_enum;
+	signal block1_column_operation		: operation_enum;
+	signal block2_column_operation		: operation_enum;
+	signal block3_column_operation		: operation_enum;
 
-	type sub_operation_enum is
+
+	type fsm_states is
 	(
-		SUB_OP_NULL,
-		SUB_OP_PLUS_ONE,
-		SUB_OP_PLUS_TWO,
-		SUB_OP_MINUS_ONE,
-		SUB_OP_MINUS_TWO,
-		SUB_OP_NEW
+		state_start,
+		state_NT_new_addresses,
+		state_writeback
 	);
-	signal sub_op_row0					: sub_operation_enum;
-	signal sub_op_row1					: sub_operation_enum;
-	signal sub_op_row2					: sub_operation_enum;
-	signal sub_op_row3					: sub_operation_enum;
-	signal sub_op_column0				: sub_operation_enum;
-	signal sub_op_column1				: sub_operation_enum;
-	signal sub_op_column2				: sub_operation_enum;
-	signal sub_op_column3				: sub_operation_enum;
+	signal state, next_state			: fsm_states := state_NT_new_addresses;
 
-	signal operation					: operation_enum;
-	signal next_operation				: operation_enum;
 
-	signal descriptor					: std_logic_vector (block_descriptor_width - 1 downto 0);
-	signal next_descriptor				: std_logic_vector (block_descriptor_width - 1 downto 0);
-	signal rotation						: rotation_enum;
-	signal next_rotation				: rotation_enum;
-	signal row0							: std_logic_vector (row_width - 1 downto 0);
-	signal row1							: std_logic_vector (row_width - 1 downto 0);
-	signal row2							: std_logic_vector (row_width - 1 downto 0);
-	signal row3							: std_logic_vector (row_width - 1 downto 0);
-	signal next_row0					: std_logic_vector (row_width - 1 downto 0);
-	signal next_row1					: std_logic_vector (row_width - 1 downto 0);
-	signal next_row2					: std_logic_vector (row_width - 1 downto 0);
-	signal next_row3					: std_logic_vector (row_width - 1 downto 0);
-	signal column0						: std_logic_vector (column_width - 1 downto 0);
-	signal column1						: std_logic_vector (column_width - 1 downto 0);
-	signal column2						: std_logic_vector (column_width - 1 downto 0);
-	signal column3						: std_logic_vector (column_width - 1 downto 0);
-	signal next_column0					: std_logic_vector (column_width - 1 downto 0);
-	signal next_column1					: std_logic_vector (column_width - 1 downto 0);
-	signal next_column2					: std_logic_vector (column_width - 1 downto 0);
-	signal next_column3					: std_logic_vector (column_width - 1 downto 0);
+	type tetrimino_select_enum is
+	(
+		TETRIMINO_OLD, TETRIMINO_NEW
+	);
+	signal tetrimino_select				: tetrimino_select_enum;
 
-	signal element_write_enable			: std_logic;
+
+	type block_select_enum is
+	(
+		BLOCK0,	BLOCK1,	BLOCK2, BLOCK3
+	);
+	signal block_select					: block_select_enum;
+
+
+	signal tetrimino_type				: std_logic_vector (tetrimino_type_width - 1 downto 0)
+										:= tetrimino_type_L_left;
+	signal tetrimino_type_next			: std_logic_vector (tetrimino_type_width - 1 downto 0);
+	signal tetrimino_we					: std_logic;
 
 begin
 
-	-------------------------------------------------------
-	---------------- determine next state -----------------
-	-------------------------------------------------------
+	process
+	(
+		tetrimino_select,
+		block0_row,    block1_row,    block2_row,    block3_row,
+		block0_column, block1_column, block2_column, block3_column
+	)
+	begin
+		case tetrimino_select is
+		when TETRIMINO_OLD =>
+			block0_row_operand		<= block0_row;
+			block0_column_operand	<= block0_column;
+			block1_row_operand		<= block1_row;
+			block1_column_operand	<= block1_column;
+			block2_row_operand		<= block2_row;
+			block2_column_operand	<= block2_column;
+			block3_row_operand		<= block3_row;
+			block3_column_operand	<= block3_column;
+		when TETRIMINO_NEW =>
+			block0_row_operand		<= default_L_left_row0;
+			block0_column_operand	<= default_L_left_column0;
+			block1_row_operand		<= default_L_left_row1;
+			block1_column_operand	<= default_L_left_column1;
+			block2_row_operand		<= default_L_left_row2;
+			block2_column_operand	<= default_L_left_column2;
+			block3_row_operand		<= default_L_left_row3;
+			block3_column_operand	<= default_L_left_column3;
+		end case;
+	end process;
 
-	with sub_op_row0			select next_row0 <=
-		default_L_left_row0			when SUB_OP_NEW,
-		row0						when others;
+	with block0_row_operation		select block0_row_next <=
+		block0_row_operand + 1			when PLUS_ONE,
+		block0_row_operand - 1			when MINUS_ONE,
+		block0_row_operand				when others; -- ZERO
+	with block1_row_operation		select block1_row_next <=
+		block1_row_operand + 1			when PLUS_ONE,
+		block1_row_operand - 1			when MINUS_ONE,
+		block1_row_operand				when others; -- ZERO
+	with block2_row_operation		select block2_row_next <=
+		block2_row_operand + 1			when PLUS_ONE,
+		block2_row_operand - 1			when MINUS_ONE,
+		block2_row_operand				when others; -- ZERO
+	with block3_row_operation		select block3_row_next <=
+		block3_row_operand + 1			when PLUS_ONE,
+		block3_row_operand - 1			when MINUS_ONE,
+		block3_row_operand				when others; -- ZERO
 
-	with sub_op_row1			select next_row1 <=
-		default_L_left_row1			when SUB_OP_NEW,
-		row1						when others;
+	with block0_column_operation	select block0_column_next <=
+		block0_column_operand + 1		when PLUS_ONE,
+		block0_column_operand - 1		when MINUS_ONE,
+		block0_column_operand			when others; -- ZERO
+	with block1_column_operation	select block1_column_next <=
+		block1_column_operand + 1		when PLUS_ONE,
+		block1_column_operand - 1		when MINUS_ONE,
+		block1_column_operand			when others; -- ZERO
+	with block2_column_operation	select block2_column_next <=
+		block2_column_operand + 1		when PLUS_ONE,
+		block2_column_operand - 1		when MINUS_ONE,
+		block2_column_operand			when others; -- ZERO
+	with block3_column_operation	select block3_column_next <=
+		block3_column_operand + 1		when PLUS_ONE,
+		block3_column_operand - 1		when MINUS_ONE,
+		block3_column_operand			when others; -- ZERO
 
-	with sub_op_row2			select next_row2 <=
-		default_L_left_row2			when SUB_OP_NEW,
-		row2						when others;
-
-	with sub_op_row3			select next_row3 <=
-		default_L_left_row3			when SUB_OP_NEW,
-		row3						when others;
-
-	with sub_op_column0			select next_column0 <=
-		default_L_left_column0		when SUB_OP_NEW,
-		column0						when others;
-
-	with sub_op_column1			select next_column1 <=
-		default_L_left_column1		when SUB_OP_NEW,
-		column1						when others;
-
-	with sub_op_column2			select next_column2 <=
-		default_L_left_column2		when SUB_OP_NEW,
-		column2						when others;
-
-	with sub_op_column3			select next_column3 <=
-		default_L_left_column3		when SUB_OP_NEW,
-		column3						when others;
-
-	next_descriptor				<= block_descriptor_L_left;
-	next_rotation				<= ROTATION_0;
-
-	-------------------------------------------------------
-	---------------- active element data ------------------
-	-------------------------------------------------------
-
+	-- 8 registers for storing new rows and columns
 	process (clock_i)
 	begin
 		if rising_edge (clock_i) then
-			if element_write_enable = '1' then
-				descriptor		<= next_descriptor;
-				rotation		<= next_rotation;
-				row0			<= next_row0;
-				row1			<= next_row1;
-				row2			<= next_row2;
-				row3			<= next_row3;
-				column0			<= next_column0;
-				column1			<= next_column1;
-				column2			<= next_column2;
-				column3			<= next_column3;
+			if new_address_write_enable = '1' then
+				block0_row_new		<= block0_row_next;
+				block0_column_new	<= block0_column_next;
+				block1_row_new		<= block1_row_next;
+				block1_column_new	<= block1_column_next;
+				block2_row_new		<= block2_row_next;
+				block2_column_new	<= block2_column_next;
+				block3_row_new		<= block3_row_next;
+				block3_column_new	<= block3_column_next;
 			end if;
 		end if;
 	end process;
 
---	process (clock_i)
---	begin
---		if rising_edge (clock_i) then
---			if operation_write_enable = '1' then
---				operation		<= next_operation;
---			end if;
---		end if;
---	end process;
+	-------------------------------------------------------
+	---------------- active tetrimino data ----------------
+	-------------------------------------------------------
+	-- 8 registers for storing active rows and columns
+	process (clock_i)
+	begin
+		if rising_edge (clock_i) then
+			if active_address_write_enable = '1' then
+				block0_row			<= block0_row_new;
+				block0_column		<= block0_column_new;
+				block1_row			<= block1_row_new;
+				block1_column		<= block1_column_new;
+				block2_row			<= block2_row_new;
+				block2_column		<= block2_column_new;
+				block3_row			<= block3_row_new;
+				block3_column		<= block3_column_new;
+			end if;
+		end if;
+	end process;
 
 	-------------------------------------------------------
 	------------------------- FSM -------------------------
@@ -284,7 +325,7 @@ begin
 	begin
 		if rising_edge (clock_i) then
 			if reset_i = '1' then
-				state <= state_new_elem;
+				state <= state_NT_new_addresses;
 			else
 				state <= next_state;
 			end if;
@@ -296,39 +337,33 @@ begin
 	process (state)
 	begin
 
-		fsm_ready_o					<= '0';
+		fsm_ready_o							<= '0';
 
-		operation					<= OP_NULL;
+		block0_row_operation				<= ZERO;
+		block1_row_operation				<= ZERO;
+		block2_row_operation				<= ZERO;
+		block3_row_operation				<= ZERO;
+		block1_column_operation				<= ZERO;
+		block1_column_operation				<= ZERO;
+		block2_column_operation				<= ZERO;
+		block3_column_operation				<= ZERO;
 
-		sub_op_row0					<= SUB_OP_NULL;
-		sub_op_row1					<= SUB_OP_NULL;
-		sub_op_row2					<= SUB_OP_NULL;
-		sub_op_row3					<= SUB_OP_NULL;
-		sub_op_column0				<= SUB_OP_NULL;
-		sub_op_column1				<= SUB_OP_NULL;
-		sub_op_column2				<= SUB_OP_NULL;
-		sub_op_column3				<= SUB_OP_NULL;
+		new_address_write_enable			<= '0';
+		active_address_write_enable			<= '0';
 
-		element_write_enable		<= '0';
-
+		tetrimino_select					<= TETRIMINO_OLD;
 
 		case state is
-		when state_new_elem =>
-			operation				<= OP_NEW;
-
-			sub_op_row0				<= SUB_OP_NEW;
-			sub_op_row1				<= SUB_OP_NEW;
-			sub_op_row2				<= SUB_OP_NEW;
-			sub_op_row3				<= SUB_OP_NEW;
-			sub_op_column0			<= SUB_OP_NEW;
-			sub_op_column1			<= SUB_OP_NEW;
-			sub_op_column2			<= SUB_OP_NEW;
-			sub_op_column3			<= SUB_OP_NEW;
-
-			element_write_enable	<= '1';
-
 		when state_start =>
-			fsm_ready_o				<= '1';
+			fsm_ready_o						<= '1';
+
+		when state_NT_new_addresses =>
+			-- all operations ZERO
+			tetrimino_select				<= TETRIMINO_NEW;
+			new_address_write_enable		<= '1';
+
+		when state_writeback =>
+			active_address_write_enable		<= '1';
 
 		when others =>
 			null;
@@ -337,20 +372,21 @@ begin
 	end process;
 
 	-- FSM next state
-	process (state,
-		block_i,
-		fsm_start_i)
+	process (state, fsm_start_i)
 	begin
 		next_state	<= state;
 
 		case state is
-		when state_new_elem =>
-			next_state <= state_start;
-
 		when state_start =>
 			if fsm_start_i = '1' then
 				next_state <= state_start;
 			end if;
+
+		when state_NT_new_addresses =>
+			next_state <= state_writeback;
+
+		when state_writeback =>
+			next_state <= state_start;
 
 		when others =>
 			next_state <= state_start;
@@ -363,18 +399,18 @@ begin
 	-------------------------------------------------------
 
 	process (
-		active_row_i, row0, row1, row2, row3,
-		active_column_i, column0, column1, column2, column3,
-		descriptor
+		active_row_i,    block0_row,    block1_row,    block2_row,    block3_row,
+		active_column_i, block0_column, block1_column, block2_column, block3_column,
+		tetrimino_type
 	) begin
-		if (active_row_i = row0 and active_column_i = column0)
-		or (active_row_i = row1 and active_column_i = column1)
-		or (active_row_i = row2 and active_column_i = column2)
-		or (active_row_i = row3 and active_column_i = column3)
+		if (active_row_i = block0_row and active_column_i = block0_column)
+		or (active_row_i = block1_row and active_column_i = block1_column)
+		or (active_row_i = block2_row and active_column_i = block2_column)
+		or (active_row_i = block3_row and active_column_i = block3_column)
 		then
-			active_data_o				<= block_descriptor_L_left;
+			active_data_o				<= tetrimino_type;
 		else
-			active_data_o				<= block_descriptor_empty;
+			active_data_o				<= tetrimino_type_empty;
 		end if;
 	end process;
 
