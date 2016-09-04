160f620a2721d32ac2101a77eb5020dde3aec964
Fedja Beader <fedja@protonmail.ch>
2016-09-04 21:58:45 +0200

    Show text "Naslednji\ntetrimino" to the left of the next tetrimino box

--- nexys4_tetris_demo/tetris_text.vhd
+++ nexys4_tetris_demo/tetris_text.vhd
@@ -55,6 +55,43 @@ begin
     end process;
 
     read_data   <= ram (to_integer (read_address_combined));
-    read_dot_o  <= font.get_dot (read_data, read_subaddress_i.row, read_subaddress_i.col);
 
+    Next_Tetrimino: block
+        type ntrom_storage is
+          array (0 to 2 ** letters.row.width - 1, 0 to 2 ** letters.col.width - 1) of
+          letter.object;
+
+        constant ntrom : ntrom_storage := (
+          1 => (
+            34     => letter.N_upper,
+            35     => letter.a,
+            36     => letter.s,
+            37     => letter.l,
+            38     => letter.e,
+            39     => letter.d,
+            40     => letter.n,
+            41     => letter.j,
+            42     => letter.i,
+            others => letter.space),
+          2 => (
+            34     => letter.t,
+            35     => letter.e,
+            36     => letter.t,
+            37     => letter.r,
+            38     => letter.i,
+            39     => letter.m,
+            40     => letter.i,
+            41     => letter.n,
+            42     => letter.o,
+            others => letter.space),
+          others => (
+            others => letter.space)
+        );
+
+        signal nt_read_data : letter.object;
+    begin
+        nt_read_data <= ntrom (to_integer (read_address_i.row), to_integer (read_address_i.col));
+        read_dot_o  <= font.get_dot (read_data, read_subaddress_i.row, read_subaddress_i.col)
+                    or font.get_dot (nt_read_data, read_subaddress_i.row, read_subaddress_i.col);
+    end block;
 end Behavioral;
