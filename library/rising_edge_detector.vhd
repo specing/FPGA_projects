6d7ba8f62324212c388cc7754f8d453473e51520
Fedja Beader <fedja@protonmail.ch>
2016-09-25 20:17:22 +0200

    rising_edge_detector.vhd: remove redundant "when others =>" and clean it up

--- library/rising_edge_detector.vhd
+++ library/rising_edge_detector.vhd
@@ -24,65 +24,40 @@ architecture Behavioral of rising_edge_detector is
         state_rising_edge,
         state_waiting_for_zero
     );
-
     signal state, next_state : state_type;
-
-    signal output   : std_logic;
-    signal input    : std_logic;
 
 begin
 
-    output_o <= output;
-    input    <= input_i;
-
-    -- FSM state change
-    process (clock_i)
+    FSM_STATE_CHANGE: process (clock_i)
     begin
         if rising_edge (clock_i) then
-            if reset_i = '1' then
-                state <= state_waiting_for_rising_edge;
-            else
-                state <= next_state;
-            end if;
+            state <= state_waiting_for_rising_edge when reset_i = '1'
+                else next_state;
         end if;
     end process;
 
-    -- FSM output
-    process (state)
+    FSM_OUTPUT: process (state)
     begin
-        case (state) is
-        when state_waiting_for_rising_edge => output <= '0';
-        when state_rising_edge             => output <= '1';
-        when state_waiting_for_zero        => output <= '0';
-        when others                        => output <= '0';
+        case state is
+        when state_waiting_for_rising_edge => output_o <= '0';
+        when state_rising_edge             => output_o <= '1';
+        when state_waiting_for_zero        => output_o <= '0';
         end case;
     end process;
 
-    -- FSM next state
-    process (state, input, input_ack_i)
+    FSM_NEXT_STATE: process (state, input_i, input_ack_i)
     begin
-        --declare default state for next_state to avoid latches
         next_state <= state;
 
-        case (state) is
+        case state is
         when state_waiting_for_rising_edge =>
-            if input = '1' then
-                next_state <= state_rising_edge;
-            end if;
+            next_state <= state_rising_edge             when input_i = '1';
 
         when state_rising_edge =>
-            if input_ack_i = '1' then
-                next_state <= state_waiting_for_zero;
-            end if;
+            next_state <= state_waiting_for_zero        when input_ack_i = '1';
 
         when state_waiting_for_zero =>
-            if input = '0' then
-                next_state <= state_waiting_for_rising_edge;
-            end if;
-
-        when others =>
-            next_state <= state_waiting_for_rising_edge;
-
+            next_state <= state_waiting_for_rising_edge when input_i = '0';
         end case;
     end process;
 
