4c3a5be3de0cab606030b64b2579634d94cd263b
Fedja Beader <fedja@protonmail.ch>
2016-09-26 14:55:18 +0200

    Move synchronisers into tactile_button

--- library/tactile_button.vhd
+++ library/tactile_button.vhd
@@ -2,14 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 
--- Note: do not instanciate this directly, but go via tactile_buttons,
--- which syncronises incoming signals for mitigation of metastability.
+-- Note: tactile_buttons has this vectored
 entity tactile_button is
     port
     (
         clock_i         : in     std_logic;
         reset_i         : in     std_logic;
-        button_sync_i   : in     std_logic;
+        button_i        : in     std_logic;
         press_ack_i     : in     std_logic;
         press_o         : out    std_logic
     );
@@ -27,8 +26,26 @@ architecture Behavioral of tactile_button is
     );
     signal state, next_state : state_type := state_waiting_for_rising_edge;
 
-begin
+    signal button_sync1 : std_logic := '0';
+    signal button_sync  : std_logic := '0';
 
+begin
+    -- sync to clock
+    SYNC: process (clock_i)
+    begin
+        if rising_edge (clock_i) then
+            if reset_i = '1' then
+                button_sync1    <= '0';
+                button_sync     <= '0';
+            else
+                button_sync1    <= button_i;
+                button_sync     <= button_sync1;
+            end if;
+        end if;
+    end process;
+    ---------------------------------------------------------------------------------------------
+    --------------------------------- Key press detection logic ---------------------------------
+    ---------------------------------------------------------------------------------------------
     FSM_STATE_CHANGE: process (clock_i)
     begin
         if rising_edge (clock_i) then
@@ -46,19 +63,19 @@ begin
         end case;
     end process;
 
-    FSM_NEXT_STATE: process (state, button_sync_i, press_ack_i)
+    FSM_NEXT_STATE: process (state, button_sync, press_ack_i)
     begin
         next_state <= state;
 
         case state is
         when state_waiting_for_rising_edge =>
-            next_state <= state_rising_edge             when button_sync_i = '1';
+            next_state <= state_rising_edge             when button_sync = '1';
 
         when state_rising_edge =>
             next_state <= state_waiting_for_zero        when press_ack_i = '1';
 
         when state_waiting_for_zero =>
-            next_state <= state_waiting_for_rising_edge when button_sync_i = '0';
+            next_state <= state_waiting_for_rising_edge when button_sync = '0';
         end case;
     end process;
 
--- library/tactile_buttons.vhd
+++ library/tactile_buttons.vhd
@@ -23,33 +23,17 @@ end tactile_buttons;
 
 
 architecture Behavioral of tactile_buttons is
-
-    signal buttons_sync : std_logic_vector(num_of_buttons - 1 downto 0);
-
 begin
-    -- sync to clock
-    process (clock_i)
-    begin
-        if rising_edge (clock_i) then
-            if reset_i = '1' then
-                buttons_sync <= (others => '0');
-            else
-                buttons_sync <= buttons_i;
-            end if;
-        end if;
-    end process;
 
-    -- generate RED
-    detectors:
-    for index in 0 to num_of_buttons - 1 generate
+    -- Individual tactile buttons
+    TBUTTONS: for index in 0 to num_of_buttons - 1 generate
     begin
-        -- Individual tactile buttons
         Inst_button: entity work.tactile_button
         port map
         (
             clock_i         => clock_i,
             reset_i         => reset_i,
-            button_sync_i   => buttons_sync (index),
+            button_i        => buttons_i (index),
             press_ack_i     => buttons_ack_i (index),
             press_o         => buttons_o (index)
         );
